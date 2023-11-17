b516a1923a919ecce54ad30427e6267b4d4ff349
Shisuya <wasbornunnamed@gmail.com>
2023-11-18 02:06:25 +0300

    pre-final version
    completing the dressing procedure

--- app/app.go
+++ app/app.go
@@ -5,13 +5,39 @@ import (
 	"backend-test/internal/config"
 	"backend-test/internal/parser"
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/sagikazarmark/slog-shim"
 )
 
-func Setup(ctx context.Context, cfg *config.Db_config, logger *slog.Logger, adapter adapter.Adapter) {
-	dump := parser.Parse(cfg.Application_cfg.Path_xml, logger)
+var isFirstRun = true
 
+func Setup(ctx context.Context, cfg *config.Db_config, logger *slog.Logger, adapter adapter.Adapter) {
+
+	exitChan := make(chan os.Signal, 1)
+	signal.Notify(exitChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Канал для периодических задач
+	ticker := time.NewTicker(1 * time.Minute)
+
+	dump := parser.Parse(cfg.Application_cfg.Path_xml, logger)
 	adapter.SaveApartments(ctx, dump, logger)
 
+	logger.Debug("Daemon start")
+
+	for {
+		select {
+		case <-exitChan:
+			logger.Debug("Signal to stop daemon")
+			ticker.Stop()
+			return
+		case <-ticker.C:
+			logger.Debug("execution of the dressing task")
+			dump = parser.Parse(cfg.Application_cfg.Path_xml, logger)
+			adapter.SaveApartments(ctx, dump, logger)
+		}
+	}
 }
